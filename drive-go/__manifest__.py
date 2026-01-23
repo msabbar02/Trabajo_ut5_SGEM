0472770a5fe8580cc289e810b62ad3c3a812d989
msabbar1 <msabbar1@educacion.navarra.es>
2026-01-23 18:48:12 +0100

    hacer el __manifest__.py

--- drive-go/__manifest__.py
+++ drive-go/__manifest__.py
@@ -1,34 +1,13 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "drive-go",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+    "name": "Gestion de reservas - GoDrive",
+    "version":"17.0.1.0.0",
+    "category": "Services",
+    "summary": "Gestion de reservas y alquiler de vehículos",
+    "depends":["base", "account"],
+    "data":[
+        "security/ir.model.access.csv",
+        "views/views.xml",
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    "application":True,
 }
 
